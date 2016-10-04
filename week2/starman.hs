7a4faa5e4c7a295a0794e182287defdbc23a53f9
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2016-10-04 23:00:02 +0300

    add starman example

---
+++ week2/starman.hs
@@ -0,0 +1,26 @@
+check :: String -> String -> Char -> (Bool,String)
+check word display c =
+  ( elem c word
+  , map (\(x,y) -> if x==c then c else y) $ zip word display
+  )
+
+turn :: String -> String -> Int -> IO ()
+turn word display n =
+  do if n==0
+       then putStrLn "You lose"
+       else if word==display
+              then putStrLn "You win!"
+              else mkguess word display n
+
+mkguess :: String -> String -> Int -> IO ()
+mkguess word display n =
+  do putStrLn (display ++ "  " ++ take n (repeat '*'))
+     putStr "  Enter your guess: "
+     q <- getLine
+     let (correct, display') = check word display (q!!0)
+     let n' = if correct then n else n-1
+     turn word display' n'
+
+starman :: String -> Int -> IO ()
+starman word n =
+  turn word (take (length word) $ repeat '-') n
